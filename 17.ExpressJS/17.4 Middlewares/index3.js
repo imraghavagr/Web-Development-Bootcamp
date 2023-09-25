4e83cce35c5c355fe3982b79868b9f05b0438508
Raghav Agarwal <raghavagrawal406@gmail.com>
2023-09-25 10:41:14 +0530

    update index3.js

--- 17.ExpressJS/17.4 Middlewares/index3.js
+++ 17.ExpressJS/17.4 Middlewares/index3.js
@@ -1,14 +1,19 @@
 import express from "express";
 
-const app = express();
-const port = 3000;
+const app = express(); 
+const port = 3000; 
+
+function logger(req, res, next){
+  console.log(req.method);
+  console.log(req.url);
+  next();
+}
 
 app.use(logger);
 
 app.get("/", (req, res) => {
-  res.send("Hello");
+  res.send("<h1>How are you?</h1>");
 });
-
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+app.listen(port, ()=>{
+  console.log(`server running on port ${port}`);
 });
