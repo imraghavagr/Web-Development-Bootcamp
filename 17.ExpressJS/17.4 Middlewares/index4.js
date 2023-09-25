5baa58498a012a6ae2e40bf294be40e2b84dbe01
Raghav Agarwal <raghavagrawal406@gmail.com>
2023-09-25 11:07:40 +0530

    add initial files for 17.4

--- 17.ExpressJS/17.4 Middlewares/index4.js
+++ 17.ExpressJS/17.4 Middlewares/index4.js
@@ -1,8 +1,30 @@
 import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
 
-const app = express();
-const port = 3000;
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const port = 3000; 
+const app = express(); 
+var brandName;
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+function generateBrandName(req, res, next){
+  console.log(req.body); 
+  brandName = req.body.street + req.body.pet;
+  next();
+}
+
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(generateBrandName);
+
+app.get("/", (req, res)=>{  
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/submit", (req, res) => {
+  res.send(`<h1>Your brand name is </h1> <h2>${brandName} 🥸</h2>`);
+});
+
+app.listen(port, ()=>{
+  console.log("Server running on port 3000");
 });
