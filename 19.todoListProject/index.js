cf5e44c4fce9824b74d4debd048b4bb79e2df1ab
Raghav Agarwal <raghavagrawal406@gmail.com>
2023-10-08 16:42:26 +0530

    udate index.js

--- 19.todoListProject/index.js
+++ 19.todoListProject/index.js
@@ -1,16 +1,31 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const port = 3000; 
 const app = express(); 
+var tasksList = []; 
+
+
 
 //enable access of static files placed inside public folder
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //handle homepage get request
 app.get("/", (req, res) => {
+    tasksList = [];
     res.render("index.ejs");
 });
+app.post("/", (req, res) => {
 
+    let newTask = req.body["newItem"];
+    //add new item into the tasksList 
+    tasksList.push(newTask);
+    const data = {
+        newTasksList : tasksList
+    };
+    res.render("index.ejs", data);
+});
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
