a85c958cbb3354cd61c8ba8006ee05471bc2b4f2
Raghav Agarwal <raghavagrawal406@gmail.com>
2023-10-09 20:10:48 +0530

    update index.js

--- 19.todoListProject/index.js
+++ 19.todoListProject/index.js
@@ -5,6 +5,21 @@ const port = 3000;
 const app = express(); 
 var tasksList = []; 
 
+const date = new Date(); 
+let year = date.getFullYear(); 
+let monthNum = date.getMonth(); 
+let day = date.getDate(); 
+const full_day_name = date.toLocaleDateString('default', { weekday: 'long' });
+
+console.log(monthNum);
+const monthMapping = {
+    0: "January", 1: "February", 2: "March", 3: "April", 4: "May", 5: "June", 
+    6: "July", 7: "August", 8: "September", 9: "October", 10: "November", 11: "December"
+}
+
+console.log(date);
+console.log(full_day_name)
+
 
 
 //enable access of static files placed inside public folder
@@ -17,12 +32,16 @@ app.get("/", (req, res) => {
     res.render("index.ejs");
 });
 app.post("/", (req, res) => {
-
     let newTask = req.body["newItem"];
     //add new item into the tasksList 
-    tasksList.push(newTask);
+    if(newTask.length > 0){
+        tasksList.push(newTask);
+    }    
     const data = {
-        newTasksList : tasksList
+        newTasksList : tasksList, 
+        dayName : full_day_name, 
+        day : day, 
+        monthName : monthMapping[monthNum]
     };
     res.render("index.ejs", data);
 });
