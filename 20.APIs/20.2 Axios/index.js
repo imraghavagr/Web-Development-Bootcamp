e240e815b10e9529862eef295158cdfce18a89e8
Raghav Agarwal <raghavagrawal406@gmail.com>
2023-10-13 12:55:08 +0530

    update index.js

--- 20.APIs/20.2 Axios/index.js
+++ 20.APIs/20.2 Axios/index.js
@@ -4,7 +4,7 @@ import axios from "axios";
 
 const app = express();
 const port = 3000;
-
+const api = "https://bored-api.appbrewery.com";
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -13,9 +13,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //   JSON data from response.data and edit the index.ejs file accordingly.
 app.get("/", async (req, res) => {
   try {
-    const response = await axios.get("https://bored-api.appbrewery.com/random");
+    const response = await axios.get(api + '/random');
+    
     const result = response.data;
-    res.render("index.ejs", { data: result });
+    console.log(result);
+    const data = {
+      activity : result.activity, 
+      type : result.type, 
+      participants : result.participants, 
+    }
+    res.render("index.ejs", data);
   } catch (error) {
     console.error("Failed to make request:", error.message);
     res.render("index.ejs", {
@@ -24,12 +31,39 @@ app.get("/", async (req, res) => {
   }
 });
 
-app.post("/", async (req, res) => {
-  console.log(req.body);
+function randomIndex(arr){
+  return arr[(Math.floor(Math.random() * arr.length))]
+}
 
-  // Step 2: Play around with the drop downs and see what gets logged.
-  // Use axios to make an API request to the /filter endpoint. Making
-  // sure you're passing both the type and participants queries.
+app.post("/", async (req, res) => {
+  try{
+    console.log(req.body);
+    let act_type = req.body.type;
+    let participants_cnt = req.body.participants;
+    if(!act_type) act_type = "";
+    if(!participants_cnt) participants_cnt = "";
+    let requestURL = api + "/filter?type=" + act_type +"&participants"+"="+participants_cnt;
+    console.log(requestURL);
+    // Step 2: Play around with the drop downs and see what gets logged.
+    // Use axios to make an API request to the /filter endpoint. Making
+    // sure you're passing both the type and participants queries.
+    const response = await axios.get(requestURL);    
+    const result = response.data;
+    let idx = randomIndex(result);
+    console.log(idx);
+    const data = {
+      activity : result[idx].activity, 
+      type : result[idx].type, 
+      participants : result[idx].participants, 
+    }
+    res.render("index.ejs", data);
+  }catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+      error: error.message,
+    });
+  }
+
   // Render the index.ejs file with a single *random* activity that comes back
   // from the API request.
   // Step 3: If you get a 404 error (resource not found) from the API request.
